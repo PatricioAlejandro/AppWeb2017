f504b82570cae0bbf08af48442e9bfe3b389236c
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-05-22 19:32:29 -0500

    Variables js

---
+++ 04-Javascript/Variables/Variables.js
@@ -0,0 +1,26 @@
+var texto = "";
+var texto2 = '';
+var num = 1;
+var bool = true;
+var dec = 11.2;
+var fecha = new Date();
+var obj = {};
+texto = 2;
+texto = obj;
+//sdgdgfgssdfgdsfgfgsdgfsdfg
+/*dfsgsdgsdfg
+ sdfgsdfgsfdg*/
+var arreglo = ["a",2.6,"c",[false,"string"],1,true];
+
+var json = {
+    nombre: "pato",
+    apellido: "chvz",
+    fechaNacimiento: new Date(),
+    mayorEdad: true,
+    peso: 70.3,
+    altura: 159
+};
+
+
+json.altura //159
+json.apellido //chvz
