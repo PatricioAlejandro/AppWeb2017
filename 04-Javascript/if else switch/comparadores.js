006b0ca3fbfeb073c8589d627db5f6cfd17bd3ef
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-05-22 19:47:04 -0500

    If

---
+++ 04-Javascript/if else switch/comparadores.js
@@ -0,0 +1,42 @@
+/**
+ * Created by patriciochavez on 5/22/17.
+ */
+var edad = true;
+if (verdad === true) {
+
+}else{
+
+}
+var num;//undefined
+if (num) {//undefined
+
+}else{
+
+}
+var caso = 2;
+if(caso){
+
+}else{
+
+}
+var caso3 = null;
+if(caso){
+
+}else{
+//devuelve falso
+}
+
+var num12 = 0;
+if(num12){
+
+}else{
+//devuelve falso
+}
+
+var num13 = [];
+if(num13){
+//devuelve verdadero
+}else{
+
+}
+
