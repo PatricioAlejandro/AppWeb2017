1fce6496677d1a79a6a246524e541c71f5eb6a8c
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-05-22 20:22:47 -0500

    funciones

--- 04-Javascript/function/funciones.js
+++ 04-Javascript/function/funciones.js
@@ -5,3 +5,29 @@ function ImprimirHolaMundo() {
     console.log("Hola Mundo")
 }
 ImprimirHolaMundo();
+
+function Imprimir(texto) {
+    console.log(texto)
+}
+Imprimir("pato");
+
+function Suma2Numeros(a,b) {
+    return a+b;
+}
+Suma2Numeros(2.3,4);
+
+function OperarNumeros(a,b,func) {
+    a = a+1;
+    b = b+2;
+    func(a,b);
+}
+
+OperarNumeros(2,4,function (primer,segundo) {
+     return primer*segundo;
+});
+
+
+
+//FOR EACH
+
+
