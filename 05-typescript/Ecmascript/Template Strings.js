e377ae16945e5a02d2e07bb7f04f117a560bc012
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-05-29 19:49:34 -0500

    Typescript basico

---
+++ 05-typescript/Ecmascript/Template Strings.js
@@ -0,0 +1,12 @@
+/**
+ * Created by patriciochavez on 5/29/17.
+ */
+//ECMASCRIPT
+var a = 1;
+var html = "<h1>HOLA</h1>h1>" +
+    "<p>Hola Amigos</p>" +
+    "<img src='algunURL' alt=''>" +
+    "<a href='google'>IR A google</a>";
+var nombreUsuario = "Patricio Chavez";
+var htmlConTemplateStrings = "\n<h1>htmlsadfasfd</h1>h1>\n<p>" + nombreUsuario + "</p>\n<ul></ul>\n";
+console.log(htmlConTemplateStrings);
