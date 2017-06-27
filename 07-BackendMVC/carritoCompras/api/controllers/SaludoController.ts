7622960387f131eebf01d11873f7d6818551dd92
Patricio Chavez <patriciochavez@Patricios-MacBook-Air.local>
2017-06-26 19:09:27 -0500

    Tarea

--- 07-BackendMVC/carritoCompras/api/controllers/SaludoController.ts
+++ 07-BackendMVC/carritoCompras/api/controllers/SaludoController.ts
@@ -18,4 +18,44 @@ module.exports = {
       return res.badRequest("Error en el metodo")
     }
   }
+  chars: function(req,res){
+    res.charset = 'utf8';
+    return res.send();
+  }
+  enviar: function(req,res){
+    res.send("Hola, este es un metodo .send")
+  }
+  son: function(req,res){
+    res.json({saludo:'Hola, este es un metodo .json'});
+  }
+  sonp: function(req,res){
+    return res.jsonp({
+      users: [{
+        name: 'Patricio',
+        id: 1
+      }, {
+        name: 'Juan'
+        id: 2
+      }]
+    });
+  }
+  tipo: function(req,res) {
+    return res.type('json');
+  }
+  formato: function (req,res) {
+    res.format({
+      text: function(){
+        res.send('not called');
+      },
+      html: function(){
+        res.send('<html><body>Got This one, but not desired</body></html>')
+      },
+      json: function(){
+        res.json({ expected: 'THIS ONE' ,but:'Not called'})
+      }
+    });
+  }
+  att: function (req,res) {
+    return res.attachment();
+  }
 }
