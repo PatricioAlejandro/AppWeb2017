eccc582eb9abe443b3a6ce8f88fa916ad96cccb3
Patricio Chavez <patricio.chavez@epn.edu.ec>
2017-07-06 08:56:40 -0500

    Remove files not necessary

--- 05-typescript/Ecmascript/map.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by patriciochavez on 5/29/17.
- */
-let arr = [4,6,7,4,5,8,10,4,7];
-//devuelve un arreglo
-let resultdao = arr.map((valor,indice,arreglo)=>{
-    return valor*0.2;
-});
-console.log("resultado ",resultdao)
